4849f9dfd9381ebee30fc829a49b78149b3534a5
hujuu <1961946+hujuu@users.noreply.github.com>
2024-01-16 20:15:19 +0900

    Update WindDirection.spec.ts

--- src/tests/WindDirection.spec.ts
+++ src/tests/WindDirection.spec.ts
@@ -1,6 +1,7 @@
 import { shallowMount } from "@vue/test-utils";
 import {describe, expect, test} from "vitest";
 import WindDirection from "../components/WindDirection.vue";
+
 describe("WindDirection", () => {
     test("renders without crashing", (): void => {
         const wrapper = shallowMount(WindDirection, {
@@ -9,5 +10,26 @@ describe("WindDirection", () => {
             },
         });
         expect(wrapper).toBeTruthy();
+    });
+    test("renders with the correct wind direction", (): void => {
+        const wrapper = shallowMount(WindDirection, {
+            props: {
+                degrees: 90,
+            },
+        });
+        const direction = wrapper.find("[data-testid=direction]");
+        expect(direction.attributes("style")).toContain("rotate(90deg)");
+        expect(direction.html()).toContain("⬇");
+    });
+
+    test("renders the correct wind direction for screen readers", (): void => {
+        const wrapper = shallowMount(WindDirection, {
+            props: {
+                degrees: 270,
+            },
+        });
+        const srOnly = wrapper.find("[data-testid=direction-sr]");
+        expect(srOnly.classes()).toContain('sr-only')
+        expect(srOnly.html()).toContain("Wind direction: 270 degrees");
     });
 });
