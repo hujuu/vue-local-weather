029d1cdf79087f7e11cbde3de792c08495f332d2
hujuu <1961946+hujuu@users.noreply.github.com>
2024-01-16 20:10:58 +0900

    Update WindDirection.vue

--- src/components/WindDirection.vue
+++ src/components/WindDirection.vue
@@ -11,8 +11,15 @@ const windStyle = computed(() => ({
 
 <template>
 <span>
-  <span aria-hidden="true" class="inline-block" :style="windStyle">⬇</span>
-  <span class="sr-only">Wind direction: {{ degrees }} degrees</span>
+  <span
+      aria-hidden="true"
+      class="inline-block"
+      data-testid="direction"
+      :style="windStyle">⬇</span>
+  <span
+      class="sr-only"
+      data-testid="direction-sr"
+  >Wind direction: {{ degrees }} degrees</span>
 </span>
 </template>
 
