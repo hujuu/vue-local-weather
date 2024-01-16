86add317531747d43adca6fab9a3b1f1096e0fef
hujuu <1961946+hujuu@users.noreply.github.com>
2024-01-16 18:00:45 +0900

    Update HelloWorld.test.ts

--- src/tests/HelloWorld.test.ts
+++ src/tests/HelloWorld.test.ts
@@ -1,8 +1,17 @@
 import HelloWorld from "../components/HelloWorld.vue";
 import {describe, expect, test} from "vitest";
+import { shallowMount, type VueWrapper } from '@vue/test-utils'
+
 
 describe("HelloWorld", () => {
     test("hello world", async () => {
         expect("HelloWorld").toBe(HelloWorld.__name)
+    })
+
+    test("Component Test", async () => {
+        const expected = "test message";
+        const wrapper = shallowMount(HelloWorld, {
+            propsData: { msg:expected }}) as VueWrapper<InstanceType<typeof HelloWorld>>
+        expect(wrapper.text()).toContain(expected)
     })
 })
